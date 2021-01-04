8f32da085e035b5b46a4a3007868de8222ad7306
Vitaly <gvj2007@gmail.com>
2021-01-04 21:11:48 +0700

    third lesson

--- js/learn.js
+++ js/learn.js
@@ -39,7 +39,29 @@ const calc = (a, b) => {
     return a + b;
 }; */
 /* ----------------------------Методы и свойства строк и чисел------------------------ */
-const str = 'test';
-const arr = [1, 2, 4];
+/* const str = 'test';
+const arr = [1, 2, 4]; */
 
-console.log(str[2] = 'd');
+/* console.log(str[2] = 'd'); */
+
+/* console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test)); */
